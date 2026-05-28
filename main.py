f1ec399baf5038d5db539eb057b2ff37c9154886
Vikas Chauhan <155131916+Vikasc97@users.noreply.github.com>
2026-05-28 22:10:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI, Request, Form, Depends
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
