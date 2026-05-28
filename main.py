b1c52f81c68fc8e248c6024195d9f067c8aa6cc5
Vikas Chauhan <155131916+Vikasc97@users.noreply.github.com>
2026-05-28 22:12:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import random
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
